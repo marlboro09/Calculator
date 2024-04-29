4e524d23e7e40ace7b7332058283907cbc444d30
Robbie <robbie331@>
2024-04-29 20:02:21 +0900

    사칙연산을 문자로 받기

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -10,6 +10,7 @@ public class App {
         int num1 = sc.nextInt();// Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         System.out.print("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();// Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-
+        System.out.print("연산자를 입력하세요 (+, -, *, /): ");
+        char op = sc.next().charAt(0); // 연산자를 문자로 입력
     }
 }
