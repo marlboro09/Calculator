5ae0f0d8813542c65d21f3f0dceab3c54a56bd50
Robbie <robbie331@>
2024-04-29 20:09:45 +0900

    입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -12,5 +12,21 @@ public class App {
         int num2 = sc.nextInt();// Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         System.out.print("연산자를 입력하세요 (+, -, *, /): ");
         char op = sc.next().charAt(0); // 연산자를 문자로 입력
+        int result = 0;
+        if (op == '+') {
+            result = num1 + num2;
+        } else if (op == '-') {
+            result = num1 - num2;
+        } else if (op == '*') {
+            result = num1 * num2;
+        } else if (op == '/') {
+            if (num2 == 0) {
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. ");
+                return;
+            }
+            result = num1 / num2;
+        }
+        System.out.println("결과: " + result);
+
     }
 }
