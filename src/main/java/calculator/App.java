2b7305de13c3fc9fa26f310673d222ba2ff59d69
Robbie <robbie331@>
2024-04-30 13:24:13 +0900

    7. 연산 결과가 10개로 고정되지 않고 무한이 저장될 수 있도록 소스 코드를 수정합니다.     - JCF(Java Collection Framework)를 사용합니다. (e.g. List, Set …)     - “remove”라는 문자열을 입력받으면 가장 먼저 저장된 결과가 삭제될 수 있도록 구현합니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,66 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
-        int[] ans = new int[10]; // 결과값을 저장할 배열
+        List<Integer> ans = new ArrayList<>(); // 결과값을 저장할 리스트
         Scanner sc = new Scanner(System.in);
 
-        int result = 0;//결과값 저장
         String input;
-        int index = 0;//결과값의 개수
 
         while (true) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int num1 = sc.nextInt();
             System.out.print("두 번째 숫자를 입력하세요: ");
             int num2 = sc.nextInt();
-            System.out.print("연산자를 입력하세요 (+, -, *, /) 또는 'exit'를 입력하여 종료하세요: ");
+            System.out.print("연산자를 입력하세요 (+, -, *, /) 또는 'exit'로 종료하거나 'remove'를 입력하여 첫 번째 결과값 삭제하세요: ");
             input = sc.next();
 
             if (input.equals("exit")) {
                 break; // exit 입력 시 반복문 종료
+            } else if (input.equals("remove")) {
+                if (!ans.isEmpty()) {
+                    int removedResult = ans.remove(0);
+                    System.out.println("삭제된 결과: " + removedResult);
+                } else {
+                    System.out.println("삭제할 결과가 없습니다.");
+                }
+                continue;
             }
 
             char op = input.charAt(0);
-            result = 0;
-            if (op == '+') {
-                result = num1 + num2;
-            } else if (op == '-') {
-                result = num1 - num2;
-            } else if (op == '*') {
-                result = num1 * num2;
-            } else if (op == '/') {
-                if (num2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. ");
+            int result;
+            switch (op) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. ");
+                        continue;
+                    }
+                    result = num1 / num2;
+                    break;
+                default:
+                    System.out.println("잘못된 입력입니다.");
                     continue;
-                }
-                result = num1 / num2;
-            } else {
-                System.out.println("잘못된 입력입니다.");
-                continue;
             }
+
             System.out.println("결과: " + result);
-
-            // 배열 이동 및 결과값 저장
-            if (index < 10) {
-                for (int i = index; i > 0; i--) {
-                    ans[i] = ans[i - 1];
-                }
-                ans[0] = result;
-                index++;
-            } else {
-                for (int i = 9; i > 0; i--) {
-                    ans[i] = ans[i - 1];
-                }
-                ans[0] = result;
-            }
-
+            ans.add(result);
 
             // 저장된 결과 값 출력
             System.out.println("저장된 결과 값:");
-            for (int i = 0; i < 10; i++) {
-                System.out.println((i + 1) + "번째 결과 : " + ans[i]);
+            for (int i = 0; i < ans.size(); i++) {
+                System.out.println((i + 1) + "번째 결과 : " + ans.get(i));
             }
         }
 
